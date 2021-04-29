a3b96c7ca47a20782c73ada6edeaf90f80e74ad8
Sertan Canpolat <58915038+scnplt@users.noreply.github.com>
2021-04-29 23:59:33 +0300

    Create screen to add expense

--- app/src/main/java/dev/sertan/android/harcamatakip/ui/onboarding/OnboardingWelcomeFragment.kt
+++ app/src/main/java/dev/sertan/android/harcamatakip/ui/onboarding/OnboardingWelcomeFragment.kt
@@ -3,19 +3,19 @@ package dev.sertan.android.harcamatakip.ui.onboarding
 import android.os.Bundle
 import android.view.View
 import androidx.navigation.fragment.findNavController
-import dev.sertan.android.harcamatakip.databinding.FragmentOnboarding1Binding
+import dev.sertan.android.harcamatakip.databinding.FragmentOnboardingWelcomeBinding
 import dev.sertan.android.harcamatakip.ui.BaseFragment
 
-class OnboardingFirstFragment : BaseFragment<FragmentOnboarding1Binding>() {
+class OnboardingWelcomeFragment : BaseFragment<FragmentOnboardingWelcomeBinding>() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         binding!!.btnSkip.setOnClickListener {
-            val action = OnboardingFirstFragmentDirections.actionOnboardingFirstFragmentToOnboardingSecondFragment()
+            val action = OnboardingWelcomeFragmentDirections.actionOnboardingFirstFragmentToOnboardingSecondFragment()
             findNavController().navigate(action)
         }
     }
 
-    override fun getViewDataBinding(): FragmentOnboarding1Binding = FragmentOnboarding1Binding.inflate(layoutInflater)
+    override fun getViewDataBinding(): FragmentOnboardingWelcomeBinding = FragmentOnboardingWelcomeBinding.inflate(layoutInflater)
 }
